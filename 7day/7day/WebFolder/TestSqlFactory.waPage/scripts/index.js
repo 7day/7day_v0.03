127a8b1855b47be8d98cf15a70de36f2e4e88e83
MrGelataio <riccardodc@MEGATRON.local>
2013-11-01 21:10:58 +0100

    .

--- 7day/7day/WebFolder/TestSqlFactory.waPage/scripts/index.js
+++ 7day/7day/WebFolder/TestSqlFactory.waPage/scripts/index.js
@@ -2,6 +2,7 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button5 = {};	// @button
 	var button4 = {};	// @button
 	var button3 = {};	// @button
 	var button2 = {};	// @button
@@ -10,19 +11,26 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // eventHandlers// @lock
 
+	button5.click = function button5_click (event)// @startlock
+	{// @endlock
+		
+      $$('d1codmenu').setValue("");
+	  $$('d1desmubr').setValue("");
+	  $$('d1desmudl').setValue("");
+	  $$('d1gennote').setValue("");		
+		
+	};// @lock
+
 	button4.click = function button4_click (event)// @startlock
 	{// @endlock
 		var campi = [];
 		campi[0]="<D1CODAZIE>";
-		campi[1]="<D1CODMENU>";
 		
 		var tipo = [];	
 		tipo[0]="N";
-		tipo[1]="S";		
-		
+				
 		var valori = [];
 		valori[0]=1;
-		valori[1]="ANAG1";
 
 		d1anagmenu=testsqlfactory.selectTable(campi, tipo, valori);
 		sources.d1anagmenu.sync();
@@ -40,7 +48,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		
 		var valori = [];
 		valori[0]=1;
-		valori[1]="ANAG1";
+		valori[1]=$$('d1codmenu').getValue();
 		
 		var ok=testsqlfactory.deleteTable(campi, tipo, valori);
         
@@ -54,20 +62,25 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		campi[0]="<D1CODAZIE>";
 		campi[1]="<D1CODMENU>";
 		campi[2]="<D1DESMUBR>";
+		campi[3]="<D1DESMUDL>";
+		campi[4]="<D1GENNOTE>";
 
 		
 		
 		var tipo = [];	
-		tipo[0]="N";
+	    tipo[0]="N";
 		tipo[1]="S";
 		tipo[2]="S";
-
+		tipo[3]="S";
+		tipo[4]="S";
 		
 		
 		var valori = [];
 		valori[0]=1;
-		valori[1]="ANAG1";
-		valori[2]="AGGIONAMENTO FATTO";
+		valori[1]=$$('d1codmenu').getValue();
+		valori[2]=$$('d1desmubr').getValue();
+		valori[3]=$$('d1desmudl').getValue();
+		valori[4]=$$('d1gennote').getValue();
 
 		
 		var ok=testsqlfactory.updateTable(campi, tipo, valori);
@@ -97,10 +110,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		
 		var valori = [];
 		valori[0]=1;
-		valori[1]="ANAG1";
-		valori[2]="PROVA DELL'APICE";
-		valori[3]="CIAO 1";
-		valori[4]="CIAO 2";
+		valori[1]=$$('d1codmenu').getValue();
+		valori[2]=$$('d1desmubr').getValue();
+		valori[3]=$$('d1desmudl').getValue();
+		valori[4]=$$('d1gennote').getValue();
 		
 		var ok=testsqlfactory.insertTable(campi, tipo, valori);
         
@@ -109,6 +122,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button5", "click", button5.click, "WAF");
 	WAF.addListener("button4", "click", button4.click, "WAF");
 	WAF.addListener("button3", "click", button3.click, "WAF");
 	WAF.addListener("button2", "click", button2.click, "WAF");
