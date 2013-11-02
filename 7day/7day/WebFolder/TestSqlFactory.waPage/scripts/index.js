ce6550548a342753994c7a59044e30a966a942ea
MrGelataio <riccardodc@MEGATRON.local>
2013-11-02 15:24:27 +0100

    SQL Factory: find generica

--- 7day/7day/WebFolder/TestSqlFactory.waPage/scripts/index.js
+++ 7day/7day/WebFolder/TestSqlFactory.waPage/scripts/index.js
@@ -2,6 +2,7 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button6 = {};	// @button
 	var button5 = {};	// @button
 	var button4 = {};	// @button
 	var button3 = {};	// @button
@@ -10,6 +11,50 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button6.click = function button6_click (event)// @startlock
+	{// @endlock
+
+	  /*
+        tabella=tabelle principate da leggere
+        campislect=elenco dei campi da leggere
+        join=tabelle in join da utilizzare
+        campiand=selezione campi in and (campo, tipo, operatore, valore1)
+        campior=selezione campi in or (campo, tipo, operatore, valore1)
+        campiorder=campi di ordinamento
+      */
+	
+		var tabella="d1anagmenu";
+		var campiselect="*";
+		var campiorder="d1codazie, d1codmenu";
+		var join=[];
+		var campiand=[];
+		var campior=[];
+		var cont=0;
+		campiand[0]=new Array("d1codazie","N","=",1);
+		
+		if($$('d1codmenu').getValue()!="")
+			campior[cont++]=new Array("d1codmenu","S","=",$$('d1codmenu').getValue());		
+
+		if($$('d1desmubr').getValue()!="")
+			campior[cont++]=new Array("d1desmubr","S","like","%"+$$('d1desmubr').getValue()+"%");
+
+		if($$('d1desmudl').getValue()!="")
+			campior[cont++]=new Array("d1desmudl","S","like","%"+$$('d1desmudl').getValue()+"%");
+
+		if($$('d1gennote').getValue()!="")
+			campior[cont++]=new Array("d1gennote","S","like","%"+$$('d1gennote').getValue()+"%");
+
+       var ok=testsqlfactory.selectTableGeneric(tabella, campiselect, join, campiand, campior, campiorder);
+        if(typeof(ok)=="string")
+         alert(ok);
+        else
+        {
+       		d1anagmenu=ok;
+		    sources.d1anagmenu.sync(); 	
+        } 
+
+	};// @lock
 
 	button5.click = function button5_click (event)// @startlock
 	{// @endlock
@@ -84,6 +129,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button6", "click", button6.click, "WAF");
 	WAF.addListener("button5", "click", button5.click, "WAF");
 	WAF.addListener("button4", "click", button4.click, "WAF");
 	WAF.addListener("button3", "click", button3.click, "WAF");
